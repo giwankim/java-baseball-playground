3ed7c2816a055506d118cb712d26d2925f94e60d
Giwan Kim <giwankim@gmail.com>
2021-07-10 13:31:04 +0900

    test(study): Implement tests for string methods

--- src/test/java/study/StringTest.java
+++ src/test/java/study/StringTest.java
@@ -3,11 +3,32 @@ package study;
 import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.assertThatExceptionOfType;
 
 public class StringTest {
     @Test
     void replace() {
         String actual = "abc".replace("b", "d");
         assertThat(actual).isEqualTo("adc");
+    }
+
+    @Test
+    void split_by_comma() {
+        assertThat("1,2".split(",")).containsExactly("1","2");
+        assertThat("1".split(",")).containsExactly("1");
+    }
+
+    @Test
+    void substring() {
+        String text = "(1,2)";
+        String actual = text.substring(1, text.length() - 1);
+        assertThat(actual).isEqualTo("1,2");
+    }
+
+    @Test
+    void charAt() {
+        assertThatExceptionOfType(StringIndexOutOfBoundsException.class)
+            .isThrownBy(() -> "abc".charAt(4))
+            .withMessageMatching("String index out of range: \\d+");
     }
 }
